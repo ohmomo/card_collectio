46a299dd1ab3e541939a5f712e926c4cb4f851fe
t.ohmomo <donaldduck4369@yahoo.co.jp>
2017-05-15 22:46:59 +0900

    カード登録画面 first commit エラーメッセージの日本語化 必須validationチェック追加 入力テキストボックスの下部にエラーメッセージ出力追加実施

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def error_messages_on(model, attr, css_class = "formError")
+    error_html = ""
+
+    if model.errors[attr].blank?
+      return error_html
+    end
+
+    error_html = ""
+
+    model.errors[attr].each do |error|
+      error_html += "<div class='#{css_class}'>#{model.class.human_attribute_name(attr)}#{error}</div>"
+    end
+    return error_html.html_safe
+  end
 end
